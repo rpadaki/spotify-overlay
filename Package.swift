aea9723a2a5cf4ced8f7356c868cd496e4327b40
Roshan Padaki <roshanpadaki@gmail.com>
2025-09-10 18:50:31 -0400

    fix packaging

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .executableTarget(
             name: "SpotifyOverlay",
             path: ".",
-            exclude: ["README.md"]
+            exclude: ["README.md", "Info.plist", ".github"]
         )
     ]
 )
